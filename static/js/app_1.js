6eb2d3ee06e4dc6f13873c4649a51bb12b5d58c1
Michael McDaniel <s450807@gmail.com>
2021-07-08 15:25:04 -0500

    fixed table data issues

---
+++ static/js/app_1.js
@@ -0,0 +1,49 @@
+// importing the data from data.js
+const tableData = data;
+
+// Referencing the HTML table using d3
+var tbody = d3.select('tbody');
+
+// Creating a function build a table
+function buildTable(data) {
+    //Clear out any existing data
+    tbody.html('');
+
+    //Create a loop to go through each row
+    data.forEach((dataRow) => {
+        //Append a row to the table body
+        let row = tbody.append("tr");
+
+        //Loop through each field in the dataRow and add each value to a cell
+        Object.values(dataRow).forEach((val) => {
+          let cell = row.append("td");
+          cell.text(val);
+          }
+        );
+      });
+}
+
+function handleClick() {
+  // Grab the datetime value from the filter
+  let date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+
+   // Check to see if a date was entered and filter the
+  // data using that date.
+  if (date) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+  };
+
+   // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+};
+
+//Attach an event to list for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//build the table when the page loads
+buildTable(tableData);
